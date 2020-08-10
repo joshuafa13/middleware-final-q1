3d71a655e4a03f705132d2eba282e5ea62df0977
Josh Fang <joshuafang13@gmail.com>
2020-08-10 17:10:09 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,31 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use((req, res, next) => {
+	const time = new Date().toLocaleString('zh-tw', { timeZone: 'Asia/Taipei', hour12: false })
+
+	console.log(`${time} | ${req.method} from ${req.url}`)
+	next()
+})
+
+app.get('/', (req, res) => {
+	res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+	res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+	res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+	res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+	console.log(`App running on port ${port}`)
+})
